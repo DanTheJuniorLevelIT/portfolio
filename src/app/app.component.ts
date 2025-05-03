17a6448a3896e3546b53b10f3dd62d3615d6ead5
Dan Malayo <danielmalayo2@gmail.com>
2025-05-03 12:32:37 +0800

    Refactors app structure with component-based layout
    Replaces placeholder content with structured components for navigation, hero section, about, projects, and contact information.
    
    Improves maintainability and scalability by adopting a modular approach in the app's architecture.
    
    Adds smooth scrolling behavior for enhanced user experience.
    
    Refactors app structure with component-based layout
    
    Replaces placeholder content with modular components for navigation, hero section, about, projects, and contact information.
    
    Improves maintainability and scalability by adopting a component-driven architecture.
    
    Adds smooth scrolling behavior for enhanced user experience.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, NavbarComponent, HeroComponent, AboutComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
